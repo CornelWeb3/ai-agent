4a8dcab102953f3069087120cd03f813a6fdde99
CornelWeb3 <cornel.ionut95@yahoo.com>
2025-03-05 22:40:08 +0200

    Add clerk login, button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import AgentPulse from "./AgentPulse";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 /**
  * A sticky header that appears at the top of every page.
@@ -14,21 +16,45 @@ import AgentPulse from "./AgentPulse";
 
 function Header() {
     return (
-        <header className="sticky top-0 left-0 right-0 px-4 md:px-0 bg-white/80 backdrop-blur-sm border-b border-gray-200 z-50">
+        <header className="sticky top-0 left-0 right-0 px-4 md:px-0 bg-white/80  border-b border-gray-200 z-50">
             <div className="container mx-auto">
-                {/* Left */}
                 <div className="flex items-center justify-between h-16">
-                    <Link href="/" className="flex items-center gap-4">
-                        <AgentPulse size="small" color="blue" />
-                        <h1 className="text-xl font-semibold bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">
-                            AgentTube
-                        </h1>
-                    </Link>
-                </div>
+                    {/* Left */}
+                    <div className="flex items-center justify-between h-16">
+                        <Link href="/" className="flex items-center gap-4">
+                            <AgentPulse size="small" color="blue" />
+                            <h1 className="text-xl font-semibold bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent">
+                                AgentTube
+                            </h1>
+                        </Link>
+                    </div>
 
-            {/* Right */}
-            <div></div>
-        </div>
+                    {/* Right ->  Only render when SignedIn */}
+                    <div className="flex items-center gap-4">
+                        <SignedIn>
+                            <Link href="/manage-plan">
+                                <Button>
+                                    Manage Plan
+                                </Button>
+                            </Link>
+
+                            <div className="p-2 w-10 h-10 flex items-center justify-center rounded-full border bg-blue-100 border-blue-200">
+                                <UserButton />
+                            </div>
+                        </SignedIn>
+
+                        <SignedOut>
+                            <SignInButton>
+                                <Button
+                                    variant="ghost"
+                                    className="bg-gradient-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text">
+                                    Sign In
+                                </Button>
+                            </SignInButton>
+                        </SignedOut>
+                    </div>
+                </div>
+            </div>
         </header >
     )
 }
