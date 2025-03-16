09949bcc9cb225fbe21b06a077923541c2a93dbb
CornelWeb3 <cornel.ionut95@yahoo.com>
2025-03-16 19:25:23 +0200

    Scehmatic envs

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import { ClerkProvider } from "@clerk/nextjs";
+import { SchematicProvider } from "@schematichq/schematic-react";
+// import SchematicWrapped from "./SchematicWrapped";
+// import { ConvexClientProvider } from "./ConvexClientProvider";
+
 
 export default function ClientWrapper({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <ClerkProvider> {children} </ClerkProvider>;
+    const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
+    if (!schematicPubKey) {
+        throw new Error(
+            "No Schematic Publishable Key found. Please add it to your .env.local file."
+        );
+    }
+    return (
+        <ClerkProvider>
+            <SchematicProvider publishableKey={schematicPubKey}>
+                {children}
+            </SchematicProvider>
+        </ClerkProvider>
+    );
 }
